4b15715344870ad117bf5af1de9319ecf85d3447
GOLDsyria <albakhetabdulkader@gmail.com>
2026-02-06 14:57:16 +0300

    config.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TV_WEBHOOK_SECRET = os.getenv("TV_WEBHOOK_SECRET")
